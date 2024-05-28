57ba8c6abfe20fa0223ff555a56f64826b3d4ea6
AndresDEVega <vegaa305@gmail.com>
2024-05-28 15:30:41 -0300

    Deploy

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+/**
+ * addEvent on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  navbarToggler.classList.toggle("active");
+};
+
+addEventOnElem(navbarToggler, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  navbarToggler.classList.remove("active");
+};
+
+addEventOnElem(navbarLinks, "click", closeNav);
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    backTopBtn.classList.add("active");
+  } else {
+    backTopBtn.classList.remove("active");
+  }
+});
+function App() {}
+
+window.onload = function (event) {
+  var app = new App();
+  window.app = app;
+};
+
+App.prototype.processingButton = function (event) {
+  const btn = event.currentTarget;
+  const slickList = event.currentTarget.parentNode;
+  const track = event.currentTarget.parentNode.querySelector("#track");
+  const slick = track.querySelectorAll(".slick");
+
+  const slickWidth = slick[0].offsetWidth;
+
+  const trackWidth = track.offsetWidth;
+  const listWidth = slickList.offsetWidth;
+  let leftPosition;
+  track.style.left == ""
+    ? (leftPosition = track.style.left = 0)
+    : (leftPosition = parseFloat(track.style.left.slice(0, -2) * -1));
+
+  btn.dataset.button == "button-prev"
+    ? prevAction(leftPosition, slickWidth, track)
+    : nextAction(leftPosition, trackWidth, listWidth, slickWidth, track);
+};
+
+let prevAction = (leftPosition, slickWidth, track) => {
+  if (leftPosition > 0) {
+    console.log("entro 2");
+    track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+  }
+};
+
+let nextAction = (leftPosition, trackWidth, listWidth, slickWidth, track) => {
+  if (leftPosition < trackWidth - listWidth) {
+    track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+  }
+};
